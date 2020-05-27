907c9940472d150b84ed8eca20036a9895baef50
Dong Min Lee <leegeobuk@gmail.com>
2020-05-28 02:28:21 +0900

    Add middleware implementation

--- handlers/products.go
+++ handlers/products.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"context"
 	"log"
 	"net/http"
 	"strconv"
@@ -14,6 +15,8 @@ import (
 type Products struct {
 	l *log.Logger
 }
+
+type keyProduct struct{}
 
 // NewProduct creates new product handler
 func NewProduct(l *log.Logger) *Products {
@@ -32,13 +35,9 @@ func (p *Products) GetProducts(w http.ResponseWriter, r *http.Request) {
 
 // AddProducts adds product to products list
 func (p *Products) AddProducts(w http.ResponseWriter, r *http.Request) {
-	prod := &data.Product{}
-
-	err := prod.FromJSON(r.Body)
-	util.CheckErr(w, err, "Unable to unmarshal json", http.StatusBadRequest)
-
+	prod := r.Context().Value(keyProduct{}).(*data.Product)
 	data.AddProducts(prod)
-	p.l.Printf("Prod: %#v", prod)
+	p.l.Printf("Added Prod: %#v", prod)
 }
 
 // UpdateProduct updates the product
@@ -48,11 +47,27 @@ func (p *Products) UpdateProduct(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(vars["id"])
 	util.CheckErr(w, err, "Unable to parse id string to number", http.StatusBadRequest)
 
-	prod := &data.Product{ID: id}
+	prod := r.Context().Value(keyProduct{}).(*data.Product)
 
 	err = prod.FromJSON(r.Body)
 	util.CheckErr(w, err, "Unable to unmarshal json", http.StatusBadRequest)
 
 	err = data.UpdateProduct(id, prod)
 	util.CheckErr(w, err, "Product not found", http.StatusNotFound)
+	p.l.Printf("Update Prod: %#v", prod)
+}
+
+// MiddlewareProductAuthentication authenticates product and then proceed to next handler
+func (p *Products) MiddlewareProductAuthentication(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		prod := &data.Product{}
+
+		err := prod.FromJSON(r.Body)
+		util.CheckErr(w, err, "Unable to unmarshal JSON", http.StatusBadRequest)
+
+		ctx := context.WithValue(r.Context(), keyProduct{}, prod)
+		req := r.WithContext(ctx)
+
+		next.ServeHTTP(w, req)
+	})
 }
