9647719d27863881c93cd6b25ac796a5049f15a3
Dong Min Lee <leegeobuk@gmail.com>
2020-06-02 00:47:35 +0900

    Feat gRPC server

--- GoServer/main.go
+++ GoServer/main.go
@@ -1,78 +1,29 @@
 package main
 
 import (
-	"context"
-	"log"
-	"net/http"
+	"fmt"
+	"net"
 	"os"
-	"os/signal"
-	"time"
 
-	"github.com/go-openapi/runtime/middleware"
-	gohandlers "github.com/gorilla/handlers"
-	"github.com/gorilla/mux"
 	"github.com/leegeobuk/GoServer/GoServer/env"
-	"github.com/leegeobuk/GoServer/GoServer/handlers"
+	"github.com/leegeobuk/GoServer/GoServer/pb/currency"
+	"github.com/leegeobuk/GoServer/GoServer/server"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 )
 
 func main() {
-	l := log.New(os.Stdout, "product-api", log.LstdFlags)
+	gs := grpc.NewServer()
+	cs := server.NewCurrency()
 
-	ph := handlers.NewProduct(l)
+	currency.RegisterCurrencyServer(gs, cs)
+	reflection.Register(gs)
 
-	mux := mux.NewRouter()
-
-	// handlers for API
-	getRouter := mux.Methods(http.MethodGet).Subrouter()
-	getRouter.HandleFunc("/products", ph.ListAll)
-	getRouter.HandleFunc("/products/{id:[0-9]+}", ph.ListSingle)
-
-	postRouter := mux.Methods(http.MethodPost).Subrouter()
-	postRouter.HandleFunc("/products", ph.AddProducts)
-	postRouter.Use(ph.MiddlewareProductAuthentication)
-
-	putRouter := mux.Methods(http.MethodPut).Subrouter()
-	putRouter.HandleFunc("/products", ph.Update)
-	putRouter.Use(ph.MiddlewareProductAuthentication)
-
-	deleteRouter := mux.Methods(http.MethodDelete).Subrouter()
-	deleteRouter.HandleFunc("/products/{id:[0-9]+}", ph.Delete)
-
-	// options for swagger handler
-	opts := middleware.RedocOpts{SpecURL: "/swagger.yaml"}
-	sh := middleware.Redoc(opts, nil)
-
-	getRouter.Handle("/docs", sh)
-	getRouter.Handle("/swagger.yaml", http.FileServer(http.Dir("./swagger")))
-
-	// CORS
-	ch := gohandlers.CORS(gohandlers.AllowedOrigins([]string{"http://localhost:3000"}))
-
-	s := &http.Server{
-		Addr:         env.Addr,
-		Handler:      ch(mux),
-		ErrorLog:     l,                 // set the logger for the server
-		ReadTimeout:  5 * time.Second,   // max time to read request from the client
-		WriteTimeout: 10 * time.Second,  // max time to write response to the client
-		IdleTimeout:  120 * time.Second, // max time for connections using TCP Keep-Alive
+	l, err := net.Listen("tcp", env.Addr)
+	if err != nil {
+		fmt.Printf("unable to listen %w", err)
+		os.Exit(1)
 	}
 
-	go func() {
-		l.Println("Starting server on port", env.Addr[1:])
-		err := s.ListenAndServe()
-		if err != nil {
-			l.Printf("Error starting server: %s\n", err)
-			os.Exit(1)
-		}
-	}()
-
-	sigChan := make(chan os.Signal)
-	signal.Notify(sigChan, os.Interrupt)
-	signal.Notify(sigChan, os.Kill)
-
-	sig := <-sigChan
-	l.Println("Received terminate, shutting down gracefully:", sig)
-
-	tc, _ := context.WithTimeout(context.Background(), 30*time.Second)
-	s.Shutdown(tc)
+	gs.Serve(l)
 }
